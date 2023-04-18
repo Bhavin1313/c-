71595545d2f7452f1c5a634d3a68d00b9c681c94
Bhavin1313 <99348404+Bhavin1313@users.noreply.github.com>
2023-04-18 03:07:34 -0700

    Polymorphism

---
+++ lec 7.2/18b01.cpp
@@ -0,0 +1,45 @@
+/*1. WAP to overload < operator to find which object
+contains higher value from given 2 numbers.*/
+
+#include<iostream>
+using namespace std;
+
+class A{
+	int a,b,c;
+	
+	public : 
+	void input()
+	{
+		cout<<"Enter a :- ";
+		cin>>a;
+	}
+	void input1()
+	{
+		cout<<"Enter b :- ";
+		cin>>b;
+	}
+	A operator < (A sahas)
+	{
+		A i;
+		if(a<sahas.b)
+		{
+			cout<<"B is big";
+		}
+		else
+		{
+			cout<<"A is big";
+		}
+		return i;
+	}
+
+};
+
+main()
+{
+	A a1;
+	a1.input();
+	A a2;
+	a2.input1();
+	A a3;
+	a3=a1<a2;
+}
