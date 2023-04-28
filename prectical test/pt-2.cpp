704af00661f7fd1c30003e68b95e5f3cdedaf79f
Bhavin1313 <99348404+Bhavin1313@users.noreply.github.com>
2023-04-28 03:29:07 -0700

    practical test

--- prectical test/pt-2.cpp
+++ prectical test/pt-2.cpp
@@ -43,7 +43,7 @@ int main() {
    
      for(i=0;i<r;i++) {
         for(j=0;j<c;j++) {
-        	if(matrix[i][j]=="  HAT ")
+        	if(matrix[i][j]=="    HAT ")
         	{
         		matrix[i][j]="   THROW";	
 			}
